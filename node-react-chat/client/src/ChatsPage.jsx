746005cfdf8062b6eff4cbc1c9a939b461d5a6b3
slunecnicee <129013318+slunecnicee@users.noreply.github.com>
2023-09-22 08:35:23 +0400

    initial commit

---
+++ node-react-chat/client/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={"a1f83f3d-f561-481b-8600-365553a8e8e5"}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
